39d2c5a42dfedee7813f7d4cd851c81e1b659b01
Julian Ibarra <julian.ibarra@somagrupo.com.br>
2025-09-14 14:42:16 -0300

    UPDATE

--- packages/core/src/Layout.tsx
+++ packages/core/src/Layout.tsx
@@ -1,8 +1,6 @@
 import { useMemo, type PropsWithChildren, type ReactElement } from 'react'
 
 import { usePageViewEvent } from './sdk/analytics/hooks/usePageViewEvent'
-import '@soma-vertical-web/core-components/styles'
-import '@soma-vertical-web/foundations/src/styles/_root.scss'
 
 function Layout({ children }: PropsWithChildren) {
   const props = useMemo(() => (children as ReactElement)?.props, [])
